52ac15c835edb7f1d6b5aa7f6a4b8dbaa9de238d
Nitin Singh <nitin.singh2020@gmail.com>
2015-05-19 20:46:46 +0100

    Add starter code for problem 5

---
+++ Lab-1/exercise5.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+template <typename BidirectionalIterator>
+void my_reverse(BidirectionalIterator first, BidirectionalIterator last) {
+    // Your implementation here!
+}
+
+int main() {
+    
+}
