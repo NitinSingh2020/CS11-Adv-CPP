a726c5748fb9213a1ab249feed833731e8bd5218
Nitin Singh <nitin.singh2020@gmail.com>
2015-05-19 20:55:46 +0100

    Add random number generator

--- Lab-1/exercise5.cc
+++ Lab-1/exercise5.cc
@@ -6,6 +6,17 @@ void my_reverse(BidirectionalIterator first, BidirectionalIterator last) {
     // Your implementation here!
 }
 
+/* Function Generator: Generates random integer in [1, 100] */
+int randomNumber() {return (std::rand()%100 + 1); }
+
+/* Helper Function Prototype */
+void test(int size);
+
 int main() {
     
+}
+
+/* Helper Function Definition */
+void test(int size) {
+
 }
