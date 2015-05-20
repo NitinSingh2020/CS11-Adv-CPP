ffd0e1703e556925df8dd93a1561312812b24e06
Nitin Singh <nitin.singh2020@gmail.com>
2015-05-20 22:25:56 +0100

    Add method definitions for Assignement operator, compd sum/diff operator

--- Lab-2/Vector3F.cc
+++ Lab-2/Vector3F.cc
@@ -1,6 +1,6 @@
-#include "Vector3F.hh"
 #include <iostream>
 #include <cassert>
+#include "Vector3F.hh"
 
 /* Default (aka no-argument) constructor */
 Vector3F::Vector3F() {
@@ -14,4 +14,46 @@ Vector3F::Vector3F(float x, float y, float z) {
     elems[0] = x;
     elems[1] = y;
     elems[2] = z;
+}
+
+/* Assignment Operator */
+Vector3F & Vector3F::operator=(const Vector3F &v) {
+
+    // Avoid self-assignment
+    if (this != &v)
+        return *this;
+}
+
+/* Compound Sum Operator */
+Vector3F & Vector3F::operator+=(const Vector3F &v) {
+    // Avoid self-assignment
+    if (this != &v) {
+        this->elems[0] = this->elems[0] + v.elems[0];
+        this->elems[1] = this->elems[1] + v.elems[1];
+        this->elems[2] = this->elems[2] + v.elems[2];
+    } else {
+        // First, make a copy of myself
+        Vector3F newCopy(*this);
+        this->elems[0] = newCopy.elems[0] + v.elems[0];
+        this->elems[1] = newCopy.elems[1] + v.elems[1];
+        this->elems[2] = newCopy.elems[2] + v.elems[2];
+    }
+    return *this;
+}
+
+/* Compound Subtraction Operator */
+Vector3F & Vector3F::operator-=(const Vector3F &v) {
+    // Avoid self-assignment
+    if (this != &v) {
+        this->elems[0] = this->elems[0] - v.elems[0];
+        this->elems[1] = this->elems[1] - v.elems[1];
+        this->elems[2] = this->elems[2] - v.elems[2];
+    } else {
+        // First, make a copy of myself
+        Vector3F newCopy(*this);
+        this->elems[0] = newCopy.elems[0] - v.elems[0];
+        this->elems[1] = newCopy.elems[1] - v.elems[1];
+        this->elems[2] = newCopy.elems[2] - v.elems[2];
+    }
+    return *this;
 }
