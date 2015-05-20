79f28b9a821ee8e7686c07d51bd09b5ff0827880
Nitin Singh <nitin.singh2020@gmail.com>
2015-05-20 22:48:04 +0100

    Add unit test for equlaity/non-equality

--- Lab-2/test_Vector3F.cc
+++ Lab-2/test_Vector3F.cc
@@ -14,11 +14,16 @@ TEST(TestVector3F, FeatureTwo) {
     // Code to exercise the feature
 }
 
-TEST(TestVector3F, FeatureThree) {
+TEST(TestVector3F, Equality) {
+    Vector3F m(1.0, 2.0, 3.0);
+    Vector3F n(1.0, 2.0, 3.0);
+    ASSERT_FLOAT_EQ(m == n, true) << "Vector Equality Operator Failed!";
+}
+
+TEST(TestVector3F, Non_Equality) {
     Vector3F m(1.0, 2.0, 3.0);
     Vector3F n(11.0, 12.0, 13.0);
-    n = m;
-    ASSERT_FLOAT_EQ(3, 3) << "length is wrong!";
+    ASSERT_FLOAT_EQ(m != n, true) << "Vector Non-Equality Oerator Failed!";
 }
 
 int main(int argc, char **argv) {
