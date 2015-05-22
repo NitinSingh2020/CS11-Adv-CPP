ce270759d59ec9a2c92056aba5a96b8fc4420316
Nitin Singh <nitin.singh2020@gmail.com>
2015-05-22 15:34:44 -0500

    Consolidate equality tests in on test case

--- Lab-2/test_Vector3F.cc
+++ Lab-2/test_Vector3F.cc
@@ -4,16 +4,12 @@
 // This is the namespace that the Google C++ Testing Framework uses.
 using namespace testing;
 
-TEST(TestVector3F, Equality_true) {
+TEST(TestVector3F, Equality) {
     Vector3F m(1.0, 2.0, 3.0);
     Vector3F n(1.0, 2.0, 3.0);
     ASSERT_FLOAT_EQ(m == n, true) << "Vector Equality Operator Failed!";
-}
-
-TEST(TestVector3F, Equality_false) {
-    Vector3F m(1.0, 2.0, 3.0);
-    Vector3F n(0.0, 0.0, 0.0);
-    ASSERT_FLOAT_EQ(m == n, false) << "Vector Equality Operator Failed!";
+    Vector3F o(0.0, 0.0, 0.0);
+    ASSERT_FLOAT_EQ(m == o, false) << "Vector Equality Operator Failed!";
 }
 
 TEST(TestVector3F, Non_Equality_true) {
